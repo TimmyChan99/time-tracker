9104b8962e13f29188759dd624f12dc39de83a91
TimmyChan99 <felemenoun@gmail.com>
2023-02-22 23:05:39 +0100

    Create time selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import DateRow from './components/DateRow';
+import TimeSelector from './components/TimeSelector';
 
 function App() {
   return (
     <div className="App">
       <DateRow />
+      <TimeSelector />
     </div>
   );
 }
--- src/components/TimeSelector.tsx
+++ src/components/TimeSelector.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState } from 'react';
 
-export const TimeSelector = () => {
-	return (
-		<div>TimeSelector</div>
-	)
+function TimeSelector() {
+  const [time, setTime] = useState(0);
+  const hours = [...Array(24).keys()].map((i) => {
+    const hour = i < 13 ? i : i - 12;
+    const meridiem = i < 12 ? 'AM' : 'PM';
+    return (
+      <option key={i} value={i}>
+        {hour}:00 {meridiem}
+      </option>
+    );
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTime(+e.target.value);
+  };
+
+  return (
+    <select required onChange={(e) => handleChange(e)}>
+      <option value="none" hidden>
+        Select time
+      </option>
+      {hours}
+    </select>
+  );
 }
 
+export default TimeSelector;

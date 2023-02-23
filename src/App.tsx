7d70ca190f766d11196bb340236941ea24f2b33e
TimmyChan99 <felemenoun@gmail.com>
2023-02-23 21:14:36 +0100

    Create a date row

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import DateRow from './components/DateRow';
-import TimeSelector from './components/TimeSelector';
 
 function App() {
   return (
     <div className="App">
       <DateRow />
-      <TimeSelector />
     </div>
   );
 }
--- src/components/DateRow.tsx
+++ src/components/DateRow.tsx
@@ -1,30 +1,42 @@
 import { useState } from 'react';
 import DateSelector from './DateSelector';
+import TimeSelector from './TimeSelector';
 
 type Tracker = {
   date: string;
   startTime: string;
   endTime: string;
-  totalHours: string;
+  totalHours: number;
   day: string;
 };
 
+const tracker = {
+  date: '',
+  startTime: '',
+  endTime: '',
+  totalHours: 0,
+  day: '',
+};
+
 function DateRow() {
-  const tracker = {
-    date: '',
-    startTime: '',
-    endTime: '',
-    totalHours: '',
-    day: '',
-  };
   const [traker, setTraker] = useState<Tracker>(tracker);
 
   return (
-    <ul>
-      <li>
-        <DateSelector />
-      </li>
-    </ul>
+    <div>
+      <DateSelector />
+      <div>
+        <span>start time</span>
+        <TimeSelector />
+      </div>
+      <div>
+        <span>end time</span>
+        <TimeSelector />
+      </div>
+      <div>
+        <span>total hours</span>
+        <span>{tracker.totalHours}</span>
+      </div>
+    </div>
   );
 }
 

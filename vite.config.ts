abc3987c319dd5bddfabc019df1ee548c5c984ef
TimmyChan99 <felemenoun@gmail.com>
2023-03-05 13:05:23 +0100

    fic linters

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-/// <reference types="vite-plugin-svgr/client" />
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
